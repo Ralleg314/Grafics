1dea56c8e2af018e4e2edc1b4215c023a9b87296
ANGEL BERGANTIÑOS YESTE <abergaye8.alumnes@ub052229.mat.ub.edu>
2017-02-28 18:16:02 +0100

    asdgsg

--- RayTracingBase/Cube.h
+++ RayTracingBase/Cube.h
@@ -6,7 +6,7 @@
 class Cube : public Object
 {
 public:
-    Cube();
+    Cube(vec3 i,vec3 e, Material *m);
     ~Cube() {}
     bool hit(const Ray& r, float t_min, float t_max, HitInfo& info) const;
 private:
