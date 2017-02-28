89175cbb7b6586efb50bd75300062ac543985391
SERGI HURTADO ABAD <shurtaab7.alumnes@ub052229.mat.ub.edu>
2017-02-28 18:16:42 +0100

    asd

--- RayTracingBase/Cube.cpp
+++ RayTracingBase/Cube.cpp
@@ -6,6 +6,10 @@ Cube::Cube(vec3 i,vec3 e, Material *m) : Object(m){
 }
 
 bool Cube::hit(const Ray& r, float t_min, float t_max, HitInfo& rec)const{
-
+    vec3 n = cross(this->p2-this->p1)/length(cross(this->p2-this->p1));
+    float prod=dot(r.dirVector(),n);
+    if(abs(prod)<0.00000000001){
+        return false;
+    }
 
 }
