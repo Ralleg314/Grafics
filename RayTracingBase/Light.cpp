c50bef177c93d87e91709998c9c700ef204921eb
SERGI HURTADO ABAD <shurtaab7.alumnes@ub052229.mat.ub.edu>
2017-03-08 18:24:43 +0100

    clase_Light

--- RayTracingBase/Light.cpp
+++ RayTracingBase/Light.cpp
@@ -1,11 +1,11 @@
 #include "Light.h"
 
-Light::Light(vec3 pos)
+Light::Light(/*vec3 pos*/)
 {
-    position=pos;
+    /*position=pos;
     ambient_color = vec3(0.2, 0.0, 0.0);
     diffuse_color = vec3(0.6, 0.0, 0.0);
-    specular_color = vec3(1.0, 1.0, 1.0);
+    specular_color = vec3(1.0, 1.0, 1.0);*/
 
 }
 
--- RayTracingBase/Light.h
+++ RayTracingBase/Light.h
@@ -1,16 +1,16 @@
 #ifndef LIGHT_H
 #define LIGHT_H
 
-#include <vector>
+
 
 class Light
 {
 public:
-    Light(vec3);
+    Light(/*vec3*/);/*
     vec3 ambient_color;
     vec3 diffuse_color;
     vec3 specular_color;
-    vec3 position;
+    vec3 position;*/
 
 };
 
