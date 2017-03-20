351f5c11163ac55f12810205f07314d8ba6ac5de
ralleg314 <ralleg314@gmail.com>
2017-03-20 15:13:01 +0100

    sync

--- RayTracingBase/Metal.cpp
+++ RayTracingBase/Metal.cpp
@@ -7,11 +7,12 @@ Metal::Metal(const vec3& diffuse, const vec3& ambient, const vec3& specular, flo
     this->ambient=ambient;
     this->opacity=1;
     this->shiness=shiness;
+    this->fuzzy=0.0f;
 }
 
 bool Metal::scatter(const Ray& r_in, const HitInfo& rec, vec3& color, Ray& scattered) const  {
     vec3 target = r_in.direction - float(2)*rec.normal*dot(r_in.direction,rec.normal);
-    scattered = Ray(rec.p, target-rec.p);
+    scattered = Ray(rec.p, target+this->fuzzy*RandomInSphere());
     color = specular;
     return true;
 }
--- RayTracingBase/Metal.h
+++ RayTracingBase/Metal.h
@@ -7,6 +7,7 @@ class Metal : public Material
 {
 public:
     Metal(const vec3& diffuse, const vec3& ambient, const vec3& specular, float shiness);
+    float fuzzy;
 
     virtual bool scatter(const Ray& r_in, const HitInfo& rec, vec3& color, Ray& scattered) const;
 };
